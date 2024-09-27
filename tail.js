bbc65a726e7b8ca708bc673e91602cd24d4b23f1
Darshita Patel <pateldarshita1994@gmail.com>
2024-09-27 05:09:10 -0400

    array tail new test cases added

--- tail.js
+++ tail.js
@@ -2,24 +2,36 @@
 
 const assertEqual = function(actual, expected) {
   if(actual === expected){
-    console.log(`✅✅✅ Assertion Passed: ${actual}  === ${expected}`);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   }
   else{
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
 // Tail
 
 const tail = function(inputArr){
-  const slicedTail = inputArr.slice(1);
-  return slicedTail
+  if(inputArr.length > 1){
+    return inputArr.slice(1);
+  }else{
+    return []
+  }
 }
 
 // test cases
+let inputArray = ["Jan", "Feb", "Mar", "Apr"]
+let result = tail(inputArray);
+let result1 = tail(["Jan"]);
+let result2 = tail([]);
 
-let result = tail(["Jan", "Feb", "Mar", "Apr"]);
 assertEqual(result.length,3);
 assertEqual(result[0],"Feb");
 assertEqual(result[1],"Mar");
 assertEqual(result[2],"Apr");
+
+assertEqual(result1[0],undefined);
+assertEqual(result2[0],undefined);
+
+
+assertEqual(inputArray.length,4);
