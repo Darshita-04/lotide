cfcdb68d0f430697b7dcfb24a40746392e83d078
Darshita Patel <pateldarshita1994@gmail.com>
2024-09-28 22:13:00 -0400

    eqArray

---
+++ eqArrays.js
@@ -0,0 +1,37 @@
+
+const assertEqual = function(actual, expected) {
+  if(actual === expected){
+    console.log(`✅✅✅ Assertion Passed: ${actual}  === ${expected}`);
+  }
+  else{
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
+  }
+};
+
+
+const eqArrays = function(arr1,arr2) {
+  if(arr1.length === 0 && arr2.length === 0) {
+    return true;
+  }
+  if(arr1.length === arr2.length) {
+    for(let i = 0; i < arr1.length; i++) {
+      if(arr1[i] === arr2[i]) {
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
+  }
+  else {
+    return false;
+  }
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays([1, 2, '3'], ['1', '2', 3]), true);
+assertEqual(eqArrays([], []), true);
+assertEqual(eqArrays(["Hello"], ["Hello"]), true);
