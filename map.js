5a6c8c32c1fcadbc524f88c406fbd88b73cedafd
Darshita Patel <pateldarshita1994@gmail.com>
2024-10-03 02:37:43 -0400

    map function implemented

---
+++ map.js
@@ -0,0 +1,40 @@
+const map = function (array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+const words = ["ground", "control", "to", "major", "tom"];
+const numbers = [12,1,5,8,9];
+
+const eqArrays = function(arr1,arr2) {
+  if(arr1.length === 0 && arr2.length === 0) {
+    return true;
+  }
+  if(arr1.length !== arr2.length) {
+    return false;
+  }
+  else{
+    for(let i = 0; i < arr1.length; i++) {
+      if(arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+const assertArraysEqual  = function(arr1, arr2) {
+  if(eqArrays(arr1,arr2) === true){
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  }
+  else{
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !==  ${arr2}`);
+  }
+};
+
+
+assertArraysEqual(map(words, (word) => word[0]), ['g', 'c', 't', 'm', 't']);
+assertArraysEqual(map(words, (word) => word.length > 3), [true, true, false, true, false]);
+assertArraysEqual(map(numbers, (number) => number % 2 === 0), [true, false, false, true, false]);
